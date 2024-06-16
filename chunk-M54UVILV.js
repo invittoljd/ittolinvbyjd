83d2fb2967135f78a13b325bd89c0bdd43222c57
invittoljd <invittoljd@gmail.com>
2024-06-16 15:08:36 -0600

    Auto-generated commit

--- chunk-M54UVILV.js
+++
@@ -1,1 +0,0 @@
-import{t as n}from"./chunk-5AJDWTEN.js";import{P as i}from"./chunk-I2XZS25C.js";var c=(()=>{let t=class t{setUser(e){this.user=e}getUser(){return this.user}isAdmin(){return this.getUser()?.type==n.typeAdmin}};t.\u0275fac=function(o){return new(o||t)},t.\u0275prov=i({token:t,factory:t.\u0275fac,providedIn:"root"});let r=t;return r})();export{c as a};
