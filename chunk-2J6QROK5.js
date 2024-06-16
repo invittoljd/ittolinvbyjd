f24faf2a8f1f4a755ed3def76cb54c176226b828
invittoljd <invittoljd@gmail.com>
2024-06-16 14:08:58 -0600

    Auto-generated commit

---
+++ chunk-2J6QROK5.js
@@ -0,0 +1,1 @@
+var r=function(a){return a.Primary="alert-primary",a.Secondary="alert-secondary",a.Success="alert-success",a.Danger="alert-danger",a.Warning="alert-warning",a.Info="alert-info",a.Light="alert-light",a.Dark="alert-dark",a}(r||{});export{r as a};
